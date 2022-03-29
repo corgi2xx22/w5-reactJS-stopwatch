24a1af390aa05f78f160440593712f80eb7503e8
me time <hellentrinh@HellenTrinh-MacBook.local>
2022-03-29 23:54:14 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import "Your code here";
-import useTimer from "./useTimer";
+// import "Your code here";
+// import useTimer from "./useTimer";
 
 function App() {
   const { time, startTimer, stopTimer, resetTimer, active } = useTimer(0);
+  const handleStopButton = () => {
+    console.log("handle when stop is clicked")
+  };
+  const handleStartButton = () => {
+    console.log("handle when start is clicked")
+  };
+  const handleResetButton = () => {
+    console.log("handle when reset is clicked")
+  };
 
   return (
     <div className="App container">
       <h1>Coder Timer</h1>
       <div className="timer__wrapper">
         <div className="timer__display">
-          <p>{"Your code here"}</p>
+          <p>{19}</p>
         </div>
         <div className="button__wrapper">
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={handleStopButton}>
             Stop
           </button>
           <button
             className="button"
-            ref={"Your code here"}
-            onClick={"Your code here"}
+            // ref={"Your code here"}
+            onClick={"handleStartButton"}
           >
             Start
           </button>
-          <button className="button" onClick={"Your code here"}>
+          <button className="button" onClick={"handleResetButton"}>
             Reset
           </button>
         </div>
--- src/formatTime.js
+++ src/formatTime.js
@@ -1,8 +1,11 @@
+// function get time values when they are executed 
+// value shown will be the present time of the stopwatch
+
 export const formatTime = (time) => {
-  const getSeconds = "Your code here";
+  const getSeconds = "00";
   const minutes = "Your code here";
-  const getMinutes = "Your code here";
-  const getHours = "Your code here";
+  const getMinutes = "10";
+  const getHours = "00";
 
   return `${getHours} : ${getMinutes} : ${getSeconds}`;
 };
